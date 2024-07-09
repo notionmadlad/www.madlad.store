5f5f39cd2e61bf6573985041df76a37160a70b06
sudharshans2009 <sudharshans2009@gmail.com>
2024-07-09 18:45:57 +0530

    Responsiveness Update

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -10,22 +10,22 @@ function ProductCard({ product }) {
   const productValue = JSON.stringify(Object.values(product)).toLowerCase();
 
   return (productValue.includes(searchParams.get("search"))) && (
-    <CardContainer>
-      <CardBody className="group/card overflow- group flex cursor-pointer flex-col rounded-lg border border-border bg-card">
-        <Link href={`/gumroad/${product.Slug[0].plain_text}`}>
+    <CardContainer className="w-full">
+      <CardBody className="group/card w-full group flex cursor-pointer flex-col rounded-lg border border-border bg-card">
+        <Link className="flex md:flex-col" href={`/gumroad/${product.Slug[0].plain_text}`}>
           <CardItem
             translateZ={50}
-            className="flex w-full justify-center transition-all duration-500 group-hover:bg-transparent group-hover:p-5"
+            className="flex flex-1 md:flex-auto w-full justify-center transition-all duration-500 group-hover:bg-transparent md:group-hover:p-5"
           >
             <Image
               src={product.Image[0].file.url}
               width={264}
               height={264}
               alt="logo"
-              className="w-full rounded-t-[10px] transition-all duration-500 group-hover:rounded-lg"
+              className="w-full rounded-l-[10px] md:rounded-t-[10px] md:rounded-bl-none transition-all duration-500 group-hover:rounded-lg"
             />
           </CardItem>
-          <CardItem translateZ={25} className="flex flex-col gap-5 p-6">
+          <CardItem translateZ={25} className="flex flex-1 md:flex-auto flex-col gap-5 p-6">
             <CardItem
               translateZ={25}
               className="flex flex-col justify-between gap-2 font-semibold"
