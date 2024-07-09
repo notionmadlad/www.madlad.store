11a42a040ad8906a784c58152ec2652525095bce
sudharshans2009 <sudharshans2009@gmail.com>
2024-07-09 21:32:41 +0530

    Image Fix

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ module.exports = {
         protocol: "https",
         hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "s3.us-west-2.amazonaws.com",
+      },
     ],
+    domains: ["prod-files-secure.s3.us-west-2.amazonaws.com", "s3.us-west-2.amazonaws.com"]
   },
 };
